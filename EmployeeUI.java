49dc7c98e3d1a2d305f3222f96c4c5cff443298e
El Jean Rementas <161451799+Jhang4102@users.noreply.github.com>
2024-05-20 08:21:27 -0400

    Create EmployeeUI.java

---
+++ EmployeeUI.java
@@ -0,0 +1,13 @@
+public class EmployeeUI {
+    public void displayEmployeeInfo(Employee employee) {
+        // Display employee information on the user interface
+    }
+
+    public void captureUserInput() {
+        // Capture user input for employee data
+    }
+
+    public void performAction() {
+        // Perform an action based on user input
+    }
+}
