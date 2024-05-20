b45e30b161bec91578d759335a4336c7699f3501
El Jean Rementas <161451799+Jhang4102@users.noreply.github.com>
2024-05-20 08:20:56 -0400

    Create ReportGenerator.java

---
+++ ReportGenerator.java
@@ -0,0 +1,13 @@
+public class ReportGenerator {
+    public void generateSalaryReport() {
+        // Generate a salary report for employees
+    }
+
+    public void generatePerformanceReport() {
+        // Generate a performance report for employees
+    }
+
+    public void generateAttendanceReport() {
+        // Generate an attendance report for employees
+    }
+}
