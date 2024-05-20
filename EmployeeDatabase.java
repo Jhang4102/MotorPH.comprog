aa2080c6d1f502c403ffd175965e39b3444b0291
El Jean Rementas <161451799+Jhang4102@users.noreply.github.com>
2024-05-20 08:20:19 -0400

    Create EmployeeDatabase.java

---
+++ EmployeeDatabase.java
@@ -0,0 +1,18 @@
+public class EmployeeDatabase {
+    public void addEmployee(Employee employee) {
+        // Add an employee to the database
+    }
+
+    public void updateEmployee(Employee employee) {
+        // Update employee information in the database
+    }
+
+    public Employee retrieveEmployee(int employeeNumber) {
+        // Retrieve employee information from the database
+        return null;
+    }
+
+    public void deleteEmployee(int employeeNumber) {
+        // Delete an employee from the database
+    }
+}
