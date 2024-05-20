1407fc280342e51953adfa6dbc7562f581861623
El Jean Rementas <161451799+Jhang4102@users.noreply.github.com>
2024-05-20 08:15:50 -0400

    Create Position.java

---
+++ Position.java
@@ -0,0 +1,11 @@
+public class Position {
+    private String positionName;
+
+    public Position(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public String getPositionName() {
+        return positionName;
+    }
+}
