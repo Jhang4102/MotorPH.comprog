de38c20c0826d0c99c8f9c2af3f050cc02853544
El Jean Rementas <161451799+Jhang4102@users.noreply.github.com>
2024-05-20 08:16:37 -0400

    Create SalaryCalculator.java

---
+++ public class SalaryCalculator {     public double calculateSalary(Employee employee) {         /SalaryCalculator.java
@@ -0,0 +1,6 @@
+public class SalaryCalculator {
+    public double calculateSalary(Employee employee) {
+        // Calculate the salary of an employee based on position, allowances, and deductions
+        return employee.calculateTotalSalary();
+    }
+}
