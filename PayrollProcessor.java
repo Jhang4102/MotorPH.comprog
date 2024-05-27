0b67cac03f5b9ae604899221164c4d91661ba07a
El Jean Rementas <161451799+Jhang4102@users.noreply.github.com>
2024-05-27 08:02:33 -0400

    Update PayrollProcessor.java

--- PayrollProcessor.java
+++ PayrollProcessor.java
@@ -1,11 +1,20 @@
 import java.util.List;
-
 public class PayrollProcessor {
         public void calculatePayroll(List<Employee> employees) {
             // Calculate payroll for employees
+            for (Employee employee : employees) {
+                double totalSalary = employee.calculateTotalSalary();
+                // Add payroll calculation logic here
+                System.out.println("Payroll calculated for Employee #" + employee.getEmployeeNumber() + ": Total Salary - " + totalSalary);
+            }
         }
 
         public void generatePayStubs(List<Employee> employees) {
             // Generate pay stubs for employees
+            for (Employee employee: employees) {
+                double totalSalary = employee.calculateTotalSalary();
+                // Add pay stub generation logic here
+                System.out.println("Pay Stub for Employee #" + employee.getEmployeeNumber() + ": Total Salary - " + totalSalary);
+            }
         }
     }
