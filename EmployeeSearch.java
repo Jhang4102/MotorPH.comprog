fadda2a2a4dc40c47cda020c09a0e940eb5515cc
El Jean Rementas <161451799+Jhang4102@users.noreply.github.com>
2024-05-27 08:01:21 -0400

    Update EmployeeSearch.java

--- EmployeeSearch.java
+++ EmployeeSearch.java
@@ -2,13 +2,31 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class EmployeeSearch {
+    private List<Employee> employeeList;
+
+    public EmployeeSearch(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
     public List<Employee> searchByPosition(String positionName) {
         // Search for employees by position
-        return new ArrayList<>();
+        List<Employee> result = new ArrayList<>();
+        for (Employee employee : employeeList) {
+            if (employee.getPosition().equalsIgnoreCase(positionName)){
+                result.add(employee);
+            }
+        }
+        return result;
     }
 
     public List<Employee> searchByLastName(String lastName) {
         // Search for employees by last name
-        return new ArrayList<>();
+        List<Employee> result = new ArrayList<>();
+        for (Employee employee : employeeList) {
+            if (employee.getLastName().equalsIgnoreCase(lastName)) {
+                result.add(employee);
+            }
+        }
+        return result;
     }
 }
